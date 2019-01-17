b25395a3dee76c1d29b7b9b62a7e1c2bb67b3893
Michael Spengler <michael.spengler@sap.com>
2019-01-17 16:49:22 +0100

    pre-commit hooks introduced for tslint checks and tests

--- src/slack-interaction-builder.spec.ts
+++ src/slack-interaction-builder.spec.ts
@@ -1,30 +1,39 @@
-import { SlackInteractionBuilder } from "./slack-interaction-builder";
-import { ISlackOriginalMessage, ISlackAttachment, ISlackAction } from "./types";
+import { SlackInteractionBuilder } from "./slack-interaction-builder"
+import { ISlackAction, ISlackAttachment, ISlackOriginalMessage } from "./types"
 
-test('basic slack message', () => {
+test("basic slack message", () => {
     const slackInteractionBuilder: SlackInteractionBuilder = new SlackInteractionBuilder()
 
     const message: ISlackOriginalMessage = slackInteractionBuilder.getMessage("Hello World")
-    expect(message.text).toBe("Hello World");
-    expect(message.attachments).toEqual([]);
-});
 
-test('basic slack attachment', () => {
+    expect(message.text)
+        .toBe("Hello World")
+
+    expect(message.attachments)
+        .toEqual([])
+})
+
+test("basic slack attachment", () => {
     const slackInteractionBuilder: SlackInteractionBuilder = new SlackInteractionBuilder()
 
     const attachment: ISlackAttachment = slackInteractionBuilder.getAttachment("Hello Attachment")
-    expect(attachment.text).toBe("Hello Attachment");
-    expect(attachment.actions).toEqual([]);
-});
+    expect(attachment.text)
+        .toBe("Hello Attachment")
 
-test('basic slack action', () => {
+    expect(attachment.actions)
+        .toEqual([])
+})
+
+test("basic slack action", () => {
     const slackInteractionBuilder: SlackInteractionBuilder = new SlackInteractionBuilder()
 
     const action: ISlackAction = slackInteractionBuilder.getAction("Hello Action")
-    expect(action.text).toBe("Hello Action");
-});
 
-test('basic message with action and attachment', () => {
+    expect(action.text)
+        .toBe("Hello Action")
+})
+
+test("basic message with action and attachment", () => {
     const slackInteractionBuilder: SlackInteractionBuilder = new SlackInteractionBuilder()
 
     const actions: ISlackAction[] = []
@@ -35,10 +44,19 @@ test('basic message with action and attachment', () => {
     attachments.push(slackInteractionBuilder.getAttachment("Hello Attachment", actions))
     const message: ISlackOriginalMessage = slackInteractionBuilder.getMessage("Hello World", attachments)
 
+    expect(message.text)
+        .toBe("Hello World")
 
-    expect(message.text).toBe("Hello World");
-    expect(message.attachments.length).toEqual(1);
-    expect(message.attachments[0].actions.length).toEqual(2);
-    expect(message.attachments[0].actions[0].text).toEqual("Hello Action 1");
-    expect(message.attachments[0].actions[1].text).toEqual("Hello Action 2");
-});
+    expect(message.attachments.length)
+        .toEqual(1)
+
+    const expectedNumberofActions: number = 2
+    expect(message.attachments[0].actions.length)
+        .toEqual(expectedNumberofActions)
+
+    expect(message.attachments[0].actions[0].text)
+        .toEqual("Hello Action 1")
+
+    expect(message.attachments[0].actions[1].text)
+        .toEqual("Hello Action 2")
+})
--- src/slack-interaction-builder.ts
+++ src/slack-interaction-builder.ts
@@ -1,46 +1,53 @@
-import { ISlackOriginalMessage, ISlackAttachment, ISlackAction, IOption } from "./types"
+import { IOption, ISlackAction, ISlackAttachment, ISlackOriginalMessage } from "./types"
 
 export class SlackInteractionBuilder {
 
-    public getAction(text: string, type?: string, id?: string, name?: string, selectedOptions?: IOption[], style?: string, value?: string): ISlackAction {
-        const action = {
+    private action: ISlackAction | undefined
+    private attachment: ISlackAttachment | undefined
+    private message: ISlackOriginalMessage | undefined
+
+    public getAction(text?: string, type?: string, id?: string, name?: string,
+                     selectedOptions?: IOption[], style?: string, value?: string): ISlackAction {
+        this.action = {
             id: (id === undefined) ? "" : id,
             name: (name === undefined) ? "" : name,
             selected_options: selectedOptions,
             style: (style === undefined) ? "" : style,
             text: (text === undefined) ? "" : text,
             type: (type === undefined) ? "" : type,
-            value: (value === undefined) ? "" : value
+            value: (value === undefined) ? "" : value,
 
         }
 
-        return action
+        return this.action
     }
 
-    public getAttachment(text?: string, actions?: ISlackAction[], fallback?: string, callbackID?: string, color?: string, id?: number): ISlackAttachment {
-        const attachment: ISlackAttachment = {
+    public getAttachment(text?: string, actions?: ISlackAction[], fallback?: string,
+                         callbackID?: string, color?: string, id?: number): ISlackAttachment {
+        this.attachment = {
             actions: (actions === undefined) ? [] : actions,
             callback_id: (callbackID === undefined) ? "" : callbackID,
             color: (color === undefined) ? "" : color,
             fallback: (fallback === undefined) ? "" : fallback,
             id: (id === undefined) ? 0 : id,
-            text: (text === undefined) ? "" : text
+            text: (text === undefined) ? "" : text,
         }
 
-        return attachment
+        return this.attachment
     }
 
-    public getMessage(text: string, attachments?: ISlackAttachment[], botID?: string, subtype?: string, ts?: string, type?: string, userName?: string): ISlackOriginalMessage {
-        const message: ISlackOriginalMessage = {
+    public getMessage(text?: string, attachments?: ISlackAttachment[], botID?: string,
+                      subtype?: string, ts?: string, type?: string, userName?: string): ISlackOriginalMessage {
+        this.message = {
             attachments: (attachments === undefined) ? [] : attachments,
             bot_id: (botID === undefined) ? "" : botID,
             subtype: (subtype === undefined) ? "" : subtype,
             text: (text === undefined) ? "" : text,
             ts: (ts === undefined) ? "" : ts,
             type: (type === undefined) ? "" : type,
-            username: (userName === undefined) ? "" : userName
+            username: (userName === undefined) ? "" : userName,
         }
 
-        return message
+        return this.message
     }
 }
